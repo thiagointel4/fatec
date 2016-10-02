28de2c2cc47f7865c1e343cb76290a682a4f001a
Thiago Freitas <thiagointel4@yahoo.com.br>
2016-10-02 06:17:35 +0000

    teste

--- atualiza.py
+++ atualiza.py
@@ -3,9 +3,10 @@ import time
 
 subprocess.check_output("git remote update", shell=True)
 time.sleep(5)
-result = subprocess.check_output("git remote update", shell=True)
+result = subprocess.check_output("git status", shell=True)
 # print result
 time.sleep(5)
+# print result.find('fast-forwarded')
 if result.find('can be fast-forwarded') > 0:
     subprocess.check_output("git pull origin", shell=True)
     print "Atualizado"
