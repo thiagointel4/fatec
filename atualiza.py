eacc16943bf8f248ea9735c4270d05f4eeaec4a3
Thiago Gomes Freitas <thiagofreitas@tecsus.com.br>
2016-10-02 02:50:17 -0300

    comando para atualizar

--- atualiza.py
+++ atualiza.py
@@ -1,5 +1,11 @@
 import subprocess
+import time
 
-batcmd="git status"
-result = subprocess.check_output(batcmd, shell=True)
-print result
+subprocess.check_output("git remote update", shell=True)
+time.sleep(10)
+result = subprocess.check_output("git remote update", shell=True)
+# print result
+
+if result.find('can be fast-forwarded') > 0:
+    subprocess.check_output("git pull origin", shell=True)
+    print "Atualizado"
